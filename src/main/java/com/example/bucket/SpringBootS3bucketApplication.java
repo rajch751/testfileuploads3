c3e9ca0417f5752c4604e8820253596587cecaf0
Rajesh C R R V N R <rchrvrvn@in.ibm.com>
2018-04-08 18:05:53 +0530

    Added cresitionlas

--- src/main/java/com/example/bucket/SpringBootS3bucketApplication.java
+++ src/main/java/com/example/bucket/SpringBootS3bucketApplication.java
@@ -8,5 +8,14 @@ public class SpringBootS3bucketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootS3bucketApplication.class, args);
+		
+		/*
+		
+		amazonProperties:
+  endpointUrl: https://s3.us-east-1.amazonaws.com
+  accessKey: AKIAJXGLQRJB77KBBF5A
+  secretKey: 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
+  
+  */
 	}
 }
